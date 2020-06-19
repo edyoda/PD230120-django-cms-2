bad354b9f382bdee3dc9aacfa6942691b5d9dedb
Kunal <kunaldk665@gmail.com>
2020-06-20 00:24:43 +0530

    Custom user model

---
+++ cms/blog/models.py
@@ -0,0 +1,160 @@
+from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+
+# 1. CRUD on blog 
+# 2. Auth :
+#     1. Only author will login and authors can perform CRUD 
+#     2. Reader and Autor
+# 3. Not loggedin user can view only 3 blogs 
+# 4. Search 
+# 5. Edit profile, Login,Logout,Reset Password,Change Password  
+# 6. Adding a rich text editor 
+
+
+# Blog:
+# 1. Title 
+# 2. Category 
+# 3. Image 
+# 4. Content 
+# 5. Author 
+# 6. Status Draft/Published 
+# 7. Published Date 
+# 8. Author Description 
+# 9. Category description
+
+# Category:
+# id 
+# name 
+# description 
+
+# Author:
+# id 
+# username
+# Password
+# email 
+# name 
+# about 
+# profile_pic 
+# domain 
+
+# Post:
+# id
+# Title : CharField 
+# image 
+# status 
+# pub_date 
+# content : TextField 
+# category 
+# author 
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# class PublishedPostManager(models.Manager):
+#     def get_queryset(self):
+#         return super().get_queryset().filter(status = "P")
+
+# class CatProgManager(models.Manager):
+#     def get_queryset(self):
+#         return super().get_queryset().filter(category__name = "Programming")
+
+class PostQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(status = "P")
+
+    def programming(self):
+        return self.filter(category__name = "Programming")
+
+
+# class PostManager(models.Manager):
+#     def get_queryset(self):
+#         return PostQuerySet(self.model)
+
+#     def published(self):
+#         return self.get_queryset().published()
+
+#     def programming(self):
+#         return self.get_queryset().programming()
+
+
+class Post(models.Model):
+    statuses = [("D","Draft"),("P","Published")]
+
+    title = models.CharField(max_length = 250)
+    content = models.TextField()
+    status = models.CharField(max_length=1,choices=statuses,default="D")
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="categories")
+    image = models.ImageField(upload_to="blog/",blank = True)
+    slug = models.SlugField(unique= True,blank = True)
+    author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+
+
+    objects = models.Manager()
+    posts = PostQuerySet.as_manager()
+    # published = PublishedPostManager()
+    # prg = CatProgManager()
+
+    def __str__(self):
+        return self.title
+
+    def save(self,*args,**kwargs):
+        
+        self.slug = slugify(self.title)
+        
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse("post-detail",args=[self.slug])
+
+
+
+# slugify(title) => slug => object 
+
+#Get all the objects:
+# Post.objects.all() => Queryset 
+
+# Get one object :
+# Post.objcets.get(field__lookkuptype = "value"):
+#     Object Does not exist
+#     Multiple objects returned 
+
+# Post.objcets.filter(category = cat_obj)
+# Post.objects.filter(category__name = "name")
+
+# obj.modelname_set.all()
+# obj.related_name.all()
+
+# urls: 
+#   localhost:8000/blogs
+
+
+# def view(request):
+#     obj = Post.objects.all()
+#     return HTTPResponse 
+
+
+# https://codepen.io/learninguser/full/WNbBXWN
+
+# reverse() => str 
+# => obj 
+# post-detail => blogs/slug 
+
+
+# / => /blogs 
+
+
+# Post.posts.all() => All the objects 
+# Post.posts.published() => ALl published posts 
+
+
+
+
+
---
+++ cms/blog/views.py
@@ -0,0 +1,224 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from blog.models import Post,Category
+from blog.forms import ContactUsForm,RegisterForm,PostForm
+from django.views import View
+from django.views import generic
+from django.urls import reverse,reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin,UserPassesTestMixin
+
+# Create your views here.
+
+# def index(request):
+#     return HttpResponse("Welcome to my blog!!!")
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request,"blog/stories.html",context = {"posts":posts})
+
+class HomeView(View):
+
+    def get(self,request):
+        posts = Post.objects.all()
+        return render(request,"blog/stories.html",context = {"posts":posts})
+
+
+class PostListView(generic.ListView):
+    model = Post 
+    queryset = Post.objects.filter(status = "P")
+    context_object_name = 'posts'
+    template_name = "blog/stories.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context['categories'] = categories
+        return context
+
+# def index2(request):
+#     return render(request,"temp.html")
+
+def post_details(request,id):
+    try:
+        post = Post.objects.get(id = id)
+        return render(request,"blog/blog-post.html",context = {"post":post})
+    except:
+        return HttpResponse("Welcome to my blog!!!")
+
+class PostView(View):
+    def get(self,request,id):
+        try:
+            post = Post.objects.get(id = id)
+            return render(request,"blog/blog-post.html",context = {"post":post})
+        except:
+            return HttpResponse("Welcome to my blog!!!")
+        
+class PostDetailView(LoginRequiredMixin,generic.DetailView):
+    model = Post
+    queryset = Post.objects.filter(status = "P")
+    template_name = "blog/blog-post.html"
+    login_url = reverse_lazy('login')
+    # pk_url_kwarg = "id"
+
+def contact_us_form_view(request):
+    # print(request.method)
+    # print(request.GET)
+    if request.method == "GET":
+        form = ContactUsForm()
+        return render(request,"blog/contact-us.html",context = {"form":form})
+    else:
+
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponse("Thank you for submitting the response")
+        else:
+            print(form.errors)
+            return render(request,"blog/contact-us.html",context = {"form":form})
+        
+
+
+def register_form_view(request):
+    # print(request.method)
+    # print(request.GET)
+    if request.method == "GET":
+        form = RegisterForm()
+        return render(request,"blog/register.html",context = {"form":form})
+    else:
+
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponse("Thank you for submitting the response")
+        else:
+            print(form.errors)
+            return render(request,"blog/register.html",context = {"form":form})
+
+def post_form_view(request):
+    # print(request.method)
+    # print(request.GET)
+    print(request.FILES)
+    if request.method == "GET":
+        form = PostForm()
+        return render(request,"blog/post.html",context = {"form":form})
+    else:
+
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Thank you for submitting the response")
+        else:
+            print(form.errors)
+            return render(request,"blog/post.html",context = {"form":form})
+
+# class PostCreateView(View):
+#     def get(self,request):
+#         form = PostForm()
+#         return render(request,"blog/post.html",context = {"form":form})
+
+#     def post(self,request):
+#         form = PostForm(request.POST,request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse("Thank you for submitting the response")
+#         else:
+#             print(form.errors)
+#             return render(request,"blog/post.html",context = {"form":form})
+
+
+
+class PostCreateView(LoginRequiredMixin,PermissionRequiredMixin,generic.CreateView):
+    permission_required = 'blog.add_post'
+    login_url = reverse_lazy('login')
+    model = Post
+    # fields = ['title','content','status','category','image','author']
+    form_class = PostForm
+    template_name = "blog/post.html"
+    # success_url = reverse_lazy("post-detail")
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,PermissionRequiredMixin,UserPassesTestMixin,generic.UpdateView):
+    permission_required = 'blog.change_post'
+    login_url = reverse_lazy('login')
+    model = Post
+    # fields = ['title','content','status','category','image','author']
+    form_class = PostForm
+    template_name = "blog/post.html"
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self,*args,**kwargs):
+        post = Post.objects.get(slug = self.kwargs.get('slug'))
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+
+
+        
+
+
+class AboutUs(generic.TemplateView):
+    template_name = "404.html"
+
+# class RedirectPost(generic.RedirectView):
+#     url = reverse_lazy('home')
+
+
+
+
+
+def post_update_form_view(request,id):
+    # print(request.method)
+    # print(request.GET)
+    post = Post.objects.get(id = id)
+    
+    if request.method == "GET":
+        form = PostForm(instance = post)
+        return render(request,"blog/post.html",context = {"form":form})
+    else:
+
+        form = PostForm(request.POST,request.FILES,instance = post)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Thank you for submitting the response")
+        else:
+            print(form.errors)
+            return render(request,"blog/post.html",context = {"form":form})
+
+
+# blog 
+#     \static
+#         \blog 
+#             images 
+#                a.jpeg 
+
+# 127.0.0.1:8000/static/a.jpeg 
+
+
+# Post.objects.create(title = "",content = "")
+
+
+
+# def prinval(val1,val2,val3):
+
+
+
+# d = {"val1":10,"val2":20,"val3":30}
+# printval(**d)
+
+
+# cleaned_data => dict 
+
+
+
+# {% url 'home' %}
+
+# reverse('home')
+# reverse_lazy("home")
